a1b0d2bdef5a86f4a3365a92a9cdcd7ac9da7284
Oladitan Saliu <86123461+saliu-oladitan@users.noreply.github.com>
2025-11-28 17:50:18 +0100

    Update settings.py

--- cv_implement/settings.py
+++ cv_implement/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--%5$-xhhdzn#!!ba*9a8+0z+*j^3$$w_k*e$mssaeq0fcgphzq'
+SECRET_KEY = SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
